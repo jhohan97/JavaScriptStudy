38621eb252a43116f974b5e5663b159e64ced5d6
Jhohan David Noreña <paezjhohan97@gmail.com>
2020-08-04 17:49:58 -0500

    Modificacion del archivo for.js en el cual se agregan mas formas de uso del for

--- Looping/for.js
+++ Looping/for.js
@@ -1,6 +1,11 @@
-for (let index = 0; index < array.length; index++) {
+function intento1() {
+    for (i = 0; i < 5; i++) {  // estructura de un for con malas practicas
+        console.log("el numero es: " + i +"<br>")
+        text += "The number is " + i + "<br>";
+      }
+}
+
+
+for (let index = 0; index < array.length; index++) { // estructura de un for en una array
     const element = array[index];
 }
-for (i = 0; i < 5; i++) {
-    text += "The number is " + i + "<br>";
-  }
